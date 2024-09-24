c94199cbf0b341e484c4dfec56d47c93e9d95437
Arik Ahnaf <141381714+arikahnaf@users.noreply.github.com>
2024-09-23 22:16:59 -0400

    Fixed indentations

--- FifaWorldCupWinner.cpp
+++ FifaWorldCupWinner.cpp
@@ -2,8 +2,7 @@
 #include <map>
 #include <string>
 
-int main()
-{
+int main() {
     std::map<int, std::string> worldCupWinners =
     {
         {1930, "Uruguay"},      {1934, "Italy"},        {1938, "Italy"},     {1950, "Uruguay"},
@@ -13,17 +12,17 @@ int main()
         {2002, "Brazil"},       {2006, "Italy"},        {2010, "Spain"},     {2014, "Germany"},
         {2018, "France"},       {2022, "Argentina"}
     };
-
+    
     std::cout << "Enter the year of the FIFA World Cup: ";
     int year;
     std::cin >> year;
-
+    
     if (worldCupWinners.find(year) != worldCupWinners.end()) {
         std::cout << "The winner of the " << year << " FIFA World Cup was " << worldCupWinners[year] << "." << std::endl;
     }
     else {
         std::cout << "There was no FIFA World Cup held in " << year << "." << std::endl;
     }
-
+    
     return 0;
 }
